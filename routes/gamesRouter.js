9dd83ca8fa33aa471ab28e41aa4fdb3cd22e440f
Jerry Chang <213577976+redjeacs@users.noreply.github.com>
2025-10-25 14:01:18 +0800

    populate all games ejs

--- routes/gamesRouter.js
+++ routes/gamesRouter.js
@@ -3,7 +3,6 @@ const gamesController = require("../controllers/gamesController");
 const gamesRouter = Router();
 
 gamesRouter.get("/", gamesController.getGames);
-
 gamesRouter.get("/new", gamesController.gameCreateGet);
 
 gamesRouter.get("/:id", gamesController.getGameById);
