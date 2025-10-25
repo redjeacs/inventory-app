5155b2fe2ea3d9dae9c3eb6fe92eff0073ca0627
Jerry Chang <213577976+redjeacs@users.noreply.github.com>
2025-10-25 14:42:06 +0800

    add game and genre forms ejs

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -3,7 +3,7 @@ const { Router } = require("express");
 const indexRouter = Router();
 
 indexRouter.get("/", (req, res) => {
-  res.render("index", { title: "Gamestop" });
+  res.render("index", { title: "GameWarehouse" });
 });
 
 module.exports = indexRouter;
